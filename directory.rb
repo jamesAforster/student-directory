70bee32464c7e3a9e218be751f7f86922f5916ea
James Forster <jamesalexforster@gmail.com>
2020-06-01 17:44:51 +0100

    adds loop function to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
 student_count = 11
-# first we puts the list of students
+# let's put all of the students into an array so that we can access them
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates",
+]
+# and then print them
 puts "The students of Villains Academy"
-puts "--------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# finally, we print the total number of students
-print "Overall, we have #{student_count} great students"
+puts "-------------"
+students.each do |student|
+  puts students
+end
+# finally, we print the total
+puts "Overall, we have #{students.count} great students"
