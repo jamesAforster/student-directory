3781f06ae41bc8a24a0411a6af1136affe9fa182
James Forster <jamesalexforster@gmail.com>
2020-06-01 17:06:22 +0100

    updated to display total amount of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+# first we puts the list of students
+
 puts "The students of Villains Academy"
 puts "--------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +13,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
+print "Overall, we have "
+# it's important that print() doesn't add new line characters
+print 11
+puts " great students"
