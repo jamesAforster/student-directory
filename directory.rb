abb369fc6c05d9606cb17e87086d0e72493e6a64
James Forster <jamesalexforster@gmail.com>
2020-06-04 08:07:58 +0100

    add save function

--- directory.rb
+++ directory.rb
@@ -44,6 +44,7 @@ def print_menu
   puts "Please enter an option: "
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -59,6 +60,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -111,6 +114,16 @@ def print_footer
       puts "In total, we have #{@students.length} students."
     end
   end
+end
+
+def save_students
+  file = File.open("students.csv","w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 # nothing happens until we call the methods
