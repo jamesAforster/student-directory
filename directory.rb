2d59bd63709236e1538cadad6203621bde4514a4
James Forster <jamesalexforster@gmail.com>
2020-06-04 15:27:20 +0100

    add students to arr method

--- directory.rb
+++ directory.rb
@@ -1,44 +1,5 @@
 @students = []
-
-def input_students
-  puts "Please enter the names of the students to enter."
-  puts "To finish, just hit return."
-  # create an empty array 
-  # students = []
-  cohorts = [:january, :april, :august, :november]
-  # get the first name
-  name = STDIN.gets.chomp
-  # while the name is empty, repeat this code
-  while !name.empty? do
-    # when a valid name is entered, then asks for a valid cohort
-    puts "Please enter cohort"
-    cohort = gets.chomp.to_sym
-    while !cohort.empty? do
-      if cohorts.include?(cohort)
-        @students << {name: name, cohort: cohort}
-        cohort = ""
-      else 
-        puts "Cohort not found. Please enter a valid cohort, or press enter to skip."
-        cohort = STDIN.gets.chomp.to_sym
-      end
-    end
-      if @students.length == 1
-        puts "We now have 1 student."
-      else
-        puts "We now have #{@students.length} students."
-      end
-    puts "Please enter another name, or press enter to skip. "
-    name = gets.chomp
-  end
-end
-
-
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
-  end
-end
+@cohorts = [:january, :april, :august, :november]
 
 def print_menu
   puts "Please enter an option: "
@@ -49,10 +10,11 @@ def print_menu
   puts "9. Exit"
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -64,11 +26,43 @@ def process(selection)
     when "3"
       save_students
     when "4"
-      try_load_students
+      load_students
     when "9"
       exit
     else
       puts "Invalid selection, please try again"
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students to enter."
+  puts "To finish, just press enter."
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    puts "Please enter cohort"
+    cohort = STDIN.gets.chomp.to_sym
+    while !cohort.empty? do
+      if @cohorts.include?(cohort)
+        students_to_arr(name, cohort)
+        cohort = ""
+      else 
+        puts "Cohort not found. Please enter a valid cohort, or press enter to skip."
+        cohort = STDIN.gets.chomp.to_sym
+      end
+    end
+    puts "Please enter another name, or press enter to skip. "
+    name = STDIN.gets.chomp
+  end
+  print_total_students
+end
+
+def show_students
+  if @students.length == 0
+    puts  "We have no students."
+  else
+    print_header
+    print_all_students
+    print_total_students
   end
 end
 
@@ -78,19 +72,28 @@ def print_header
   puts "--------------------------------"
 end
 
+def print_all_students
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]})"
+  end
+  puts 
+  puts
+end
 
-def print_students_list
-  if @students.length > 0
-    puts "Please enter the cohort to group by, or press enter to see the whole list."
-    print_by_cohort
-    print_all_students
+def students_to_arr(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
+def print_total_students
+  if @students.length == 1
+    puts "We now have 1 student."
   else
-    puts "We do not have any students."
+    puts "We now have #{@students.length} students."
   end
 end
 
 def print_by_cohort
-  view_cohort = gets.chomp.to_sym
+  view_cohort = STDIN.gets.chomp.to_sym
   while !view_cohort.empty? do 
     @students.each_with_index do |student,index|
       if student[:cohort] == view_cohort
@@ -98,23 +101,7 @@ def print_by_cohort
       end
     end
     puts "Please enter another cohort to view, or press enter to see the whole list."
-    view_cohort = gets.chomp.to_sym
-  end
-end
-
-def print_all_students
-  @students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]})"
-  end
-end
-
-def print_footer
-  if !@students.empty?
-    if @students.count == 1
-      puts "In total, we have 1 student."
-    else
-      puts "In total, we have #{@students.length} students."
-    end
+    view_cohort = STDIN.gets.chomp.to_sym
   end
 end
 
@@ -132,7 +119,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename,"r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    students_to_arr(name, cohort)
   end
   file.close
 end
@@ -149,6 +136,7 @@ def try_load_students
   end
 end
 
+
+
 # nothing happens until we call the methods
-load_students
 interactive_menu
